0c37c45b95cd0ad84164dc800603c19267ca3a51
Aman Verma <verma1090aman@gmail.com>
2021-06-27 20:19:24 +0530

    Added check

--- frontend/src/components/modal.js
+++ frontend/src/components/modal.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { Button, Header, Image, Modal } from 'semantic-ui-react'
 import ReactQuill from "react-quill"
 import 'react-quill/dist/quill.snow.css'
-import  { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import './textEditor.css';
 function ModalExampleModal() {
   const [to, setTo] = useState("");
@@ -15,7 +15,7 @@ function ModalExampleModal() {
   const [interval, setInterval] = useState({ minutes: 0, seconds: 0, hours: 0, days: 0 });
   function onchangeInterval(e) {
 
-    console.log("beofre" , interval)
+    console.log("beofre", interval)
     switch (e.target.id) {
       case "intervaldays": setInterval({
         minutes: interval.minutes, seconds: interval.seconds, hours: interval.hours, days: e.target.value
@@ -37,7 +37,7 @@ function ModalExampleModal() {
         break;
       default:
     }
-    console.log("after" , interval)
+    console.log("after", interval)
   }
   function FromChange(e) {
     setFrom(e.target.value);
@@ -56,78 +56,89 @@ function ModalExampleModal() {
     console.log(cc);
   }
   function OnSubmit() {
-    
+    if (to && convertedText && subject) {
     fetch(`https://simplemailbackend.herokuapp.com/api/v1/mails/create`, {
-      method: "put",
-      headers: {
-        "Content-Type": "application/json",
-        "x-access-token": localStorage.getItem('jwt')
-      },
-      body: JSON.stringify({
-        name: "yashvi",
-        to: to,
-        subject: subject,
-        body: convertedText,
-        interval: interval,
-        cc: ["yashvimahapatra201@gmail.com"]
-      }),
-    })
-      .then((response) => response.json())
-      .then((data) => {
-        console.log(data);
+        method: "put",
+        headers: {
+          "Content-Type": "application/json",
+          "x-access-token": localStorage.getItem('jwt')
+        },
+        body: JSON.stringify({
+          name: "yashvi",
+          to: to,
+          subject: subject,
+          body: convertedText,
+          interval: interval,
+          cc: ["yashvimahapatra201@gmail.com"]
+        }),
       })
-}
+        .then((response) => {
+          if (response.status == 200) {
+            response.json().then((data) => {
+              setOpen(false)
+            });
+          }
+          else {
+            alert("Error Occurred")
+            console.log(response)
+          }
+        }).catch((er)=>{console.log(er)}) 
+    }
+    else{
+      alert("Fill all the fields");
+    }
+  }
 
 
   return (
-    <Modal 
+    <Modal
       onClose={() => setOpen(false)}
       onOpen={() => setOpen(true)}
       open={open}
-      trigger={ <Button style={{ position: "relative", right: "120px", top: "-72px" }} className="ui right floated secondary basic button">
-      <h3 style={{ textAlign: "center", cursor: "pointer" }}>Create <i class="plus icon"></i></h3>
-    </Button> }
+      trigger={<Button style={{ position: "relative", right: "120px", top: "-72px" }} className="ui right floated secondary basic button">
+        <h3 style={{ textAlign: "center", cursor: "pointer" }}>Create <i class="plus icon"></i></h3>
+      </Button>}
     >
       <Modal.Header> Schedule Mail</Modal.Header>
       <Modal.Content>
-      <div>
-      <div style={{ backgroundColor: "#EDEDED", width: "100%" }}>
-        
-        <input type="text" id="fname" placeholder="To:" name="fname"  value={to} onChange={ToChange}/>
-        <input type="text" id="fname" placeholder="Subject:" name="fname1" onChange={subjectChange}/>
-        <input type="text" id="fname" placeholder="CC:" name="fname2" />
-        <table className="interval">
-          <tbody>
-            <tr>
-              <td>
-                <input type="number" id="intervaldays" className="intervalInput" placeholder="Days" name="fname1" onChange={onchangeInterval} />
-                <input type="number" id="intervalhrs" className="intervalInput" placeholder="Hours" name="fname1" onChange={onchangeInterval} />
-                <input type="number" id="intervalminutes" className="intervalInput" placeholder="Minutes" name="fname1" onChange={onchangeInterval} />
-                <input type="number" id="intervalseconds" className="intervalInput" placeholder="Seconds" name="fname1" onChange={onchangeInterval} />
-              </td>
-            </tr>
-          </tbody>
-        </table>
-      
+        <div>
+          <div style={{ backgroundColor: "#EDEDED", width: "100%" }}>
 
-        <ReactQuill
-          theme='snow'
-          value={convertedText}
-          onChange={setConvertedText}
-          style={{ minHeight: '300px' }}
-        />
-      </div>
-    </div>
+            <input type="text" id="fname" placeholder="To:" name="fname" value={to} onChange={ToChange} />
+            <input type="text" id="fname" placeholder="Subject:" name="fname1" value={subject} onChange={subjectChange} />
+            <input type="text" id="fname" placeholder="CC:" name="fname2" />
+            <table className="interval">
+              <tbody>
+                <tr>
+                  <td>
+                    <input type="number" id="intervaldays" className="intervalInput" placeholder="Days" name="fname1" onChange={onchangeInterval} />
+                    <input type="number" id="intervalhrs" className="intervalInput" placeholder="Hours" name="fname1" onChange={onchangeInterval} />
+                    <input type="number" id="intervalminutes" className="intervalInput" placeholder="Minutes" name="fname1" onChange={onchangeInterval} />
+                    <input type="number" id="intervalseconds" className="intervalInput" placeholder="Seconds" name="fname1" onChange={onchangeInterval} />
+                  </td>
+                </tr>
+              </tbody>
+            </table>
+
+
+            <ReactQuill
+              theme='snow'
+              value={convertedText}
+              onChange={setConvertedText}
+              style={{ minHeight: '300px' }}
+            />
+          </div>
+        </div>
       </Modal.Content>
-      <Modal.Actions onClick={() => setOpen(false)}>
-        <Button color='black' >
-            Cancel
+      <Modal.Actions >
+        <Button color='black' onClick={() => setOpen(false)}>
+          Cancel
         </Button>
-     
-        <Button style={{backgroundColor:"#39A2DB",color:"#fff"}} onClick={OnSubmit }>
-           Send
+
+        <Button style={{ backgroundColor: "#39A2DB", color: "#fff" }} onClick={OnSubmit}>
+          Send
         </Button>
-    
+
       </Modal.Actions>
     </Modal>
   )
